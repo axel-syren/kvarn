fe6463012e031a273de9209759384c76d436ec97
axelsyren <dat15asy@student.lu.se>
2017-05-25 19:14:29 +0200

    Implemented Coordinate translation matrix in NodeUtility

--- src/view/util/NodeUtility.java
+++ src/view/util/NodeUtility.java
@@ -1,5 +1,31 @@
 package view.util;
 
 public class NodeUtility {
+	public static final int[][] COORDINATE = {
+			{ 0, 0 }, // index = 0
+			{ 3, 0 }, // index = 1
+			{ 6, 0 }, // index = 2
+			{ 1, 1 }, // index = 3
+			{ 3, 1 }, // index = 4
+			{ 5, 1 }, // index = 5
+			{ 2, 2 }, // index = 6
+			{ 3, 2 }, // index = 7
+			{ 4, 2 }, // index = 8
+			{ 0, 3 }, // index = 9
+			{ 1, 3 }, // index = 10
+			{ 2, 3 }, // index = 11
+			{ 4, 3 }, // index = 12
+			{ 5, 3 }, // index = 13
+			{ 6, 3 }, // index = 14
+			{ 2, 4 }, // index = 15
+			{ 3, 4 }, // index = 16
+			{ 4, 4 }, // index = 17
+			{ 1, 5 }, // index = 18
+			{ 3, 5 }, // index = 19
+			{ 5, 5 }, // index = 20
+			{ 0, 6 }, // index = 21
+			{ 3, 6 }, // index = 22
+			{ 6, 6 }, // index = 23
+	};
 
 }
